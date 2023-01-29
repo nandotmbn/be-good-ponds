d5279c16fe9f00fa585e4fff5d662f59e642a56b
nandotmbn <orlandopratama695@gmail.com>
2023-01-30 04:10:31 +0700

    fix: created add joint

--- views/point.go
+++ views/point.go
@@ -8,6 +8,13 @@ type PayloadPoint struct {
 	Temperature float32 `json:"temperature,omitempty"`
 }
 
+type LastPoint struct {
+	Acidity     float32   `json:"acidity,omitempty"`
+	Salinity    float32   `json:"salinity,omitempty"`
+	Temperature float32   `json:"temperature,omitempty"`
+	CreatedAt   time.Time `json:"created_at,omitempty" bson:"created_at,omitempty"`
+}
+
 type FinalPoint struct {
 	Id          interface{} `json:"_id,omitempty" bson:"_id,omitempty" validate:"required"`
 	Acidity     float32     `json:"acidity,omitempty"`
